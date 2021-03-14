fa72aa6ceebb79a7566521beee58dd06e1ff0652
darthpaul <paulakhamiogu@gmail.com>
2021-03-14 15:51:52 +0100

    phew: draw oval

--- app/src/main/java/com/kryptkode/facedetection/detection/FacePositionView.kt
+++ app/src/main/java/com/kryptkode/facedetection/detection/FacePositionView.kt
@@ -8,40 +8,40 @@ import androidx.annotation.ColorInt
 import androidx.annotation.ColorRes
 import androidx.core.content.ContextCompat
 import com.kryptkode.facedetection.R
+import com.sdsmdg.harjot.vectormaster.VectorMasterDrawable
 
 class FacePositionView(context: Context, attrs: AttributeSet?) : View(context, attrs) {
 
-    private val path = Path()
-    private val ovalRect = RectF()
-    private val ovalCenter = PointF()
     private val outlinePaint = Paint().apply {
-        color = Color.parseColor("#FF1B5ECE")
+        color = R.color.blue.toColorInt(context)
         style = Paint.Style.STROKE
         strokeWidth = R.dimen.stroke_width.resToPx(context)
         isAntiAlias = true
     }
 
     private val backgroundPaint = Paint().apply {
-        color = Color.parseColor("#BA000000")
+        color = R.color.background.toColorInt(context)
+        style = Paint.Style.FILL
+        isAntiAlias = true
     }
+
+    private val outlineDrawable = VectorMasterDrawable(context, R.drawable.ic_border)
+    private val backgroundPath = outlineDrawable.fullPath
+    private val outlinePath = Path(backgroundPath)
+    private val outlineMatrix = Matrix()
+    private val outlineBounds = RectF()
+
+    private val facesBounds = mutableListOf<FaceBounds>()
 
     private var showOutline = false
     private var callback: ((Boolean) -> Unit)? = null
 
-    private val facesBounds = mutableListOf<FaceBounds>()
-    private val anchorPaint = Paint()
-    private val idPaint = Paint()
-    private val boundsPaint = Paint()
-
     init {
-        anchorPaint.color = ContextCompat.getColor(context, android.R.color.holo_blue_dark)
-
-        idPaint.color = ContextCompat.getColor(context, android.R.color.holo_blue_dark)
-        idPaint.textSize = 40f
-
-        boundsPaint.style = Paint.Style.STROKE
-        boundsPaint.color = ContextCompat.getColor(context, android.R.color.holo_blue_dark)
-        boundsPaint.strokeWidth = 4f
+        outlineMatrix.reset()
+        val scale = R.dimen.stroke_scale.resToPx(context)
+        outlineMatrix.setScale(scale, scale)
+        backgroundPath.transform(outlineMatrix)
+        outlinePath.transform(outlineMatrix)
     }
 
     internal fun updateFaces(bounds: List<FaceBounds>) {
@@ -53,44 +53,29 @@ class FacePositionView(context: Context, attrs: AttributeSet?) : View(context, a
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
-        path.reset()
-
-        ovalCenter.set(
-                (width / 2).toFloat(),
-                (height / 2).toFloat()
+        backgroundPath.computeBounds(outlineBounds, true)
+        backgroundPath.offset(
+            (width / 2) - outlineBounds.centerX(),
+            (height / 2) - outlineBounds.centerY()
         )
 
-        ovalRect.set(
-                fromCenter(
-                        ovalCenter,
-                        (width * 0.45).toFloat(),
-                        (height * 0.45).toFloat()
-                )
+        outlinePath.computeBounds(outlineBounds, true)
+        outlinePath.offset(
+            (width / 2) - outlineBounds.centerX(),
+            (height / 2) - outlineBounds.centerY()
         )
 
-        path.addOval(
-                ovalRect,
-                Path.Direction.CW
-        )
+        backgroundPath.fillType = Path.FillType.INVERSE_EVEN_ODD
 
-        path.fillType = Path.FillType.INVERSE_EVEN_ODD
-
-        canvas.drawPath(path, backgroundPaint)
+        canvas.drawPath(backgroundPath, backgroundPaint)
 
         facesBounds.forEach { faceBounds ->
-            if (ovalRect.contains(faceBounds.box)) {
-                canvas.drawOval(
-                        ovalRect,
-                        outlinePaint
-                )
+            if (outlineBounds.contains(faceBounds.box)) {
+                canvas.drawPath(outlinePath, outlinePaint)
                 callback?.invoke(true)
             } else {
                 callback?.invoke(false)
             }
-        }
-
-        if(facesBounds.isEmpty()){
-            callback?.invoke(false)
         }
     }
 
@@ -100,25 +85,16 @@ class FacePositionView(context: Context, attrs: AttributeSet?) : View(context, a
     }
 
     fun isWithinOutline(rectF: RectF): Boolean {
-        return ovalRect.contains(rectF)
+        return outlineBounds.contains(rectF)
     }
 
     fun setOnOutLineShownListener(listener: (Boolean) -> Unit) {
         callback = listener
     }
 
-
 }
 
 fun Int.resToPx(context: Context): Float = context.resources.getDimension(this)
-
-fun fromCenter(center: PointF, width: Float, height: Float) =
-        RectF(
-                center.x - width / 2,
-                center.y - height / 2,
-                center.x + width / 2,
-                center.y + height / 2
-        )
 
 @ColorInt
 fun @receiver:ColorRes Int.toColorInt(context: Context): Int = ContextCompat.getColor(context, this)
